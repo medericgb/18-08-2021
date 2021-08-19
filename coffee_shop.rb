6fa6d40664c1d2e2973b784d3f7f620c8b8d376d
Médéric Gbagba <medericjoel@gmail.com>
2021-08-19 16:35:18 +0000

    Coffee Shop

--- coffee_shop.rb
+++ coffee_shop.rb
@@ -1,50 +1,44 @@
-class Coffeeshop
+class CoffeeShop
   attr_accessor :name, :menu, :orders
-
   def initialize(name, menu)
     @name = name
     @menu = menu
     @orders = []
   end
-
   def add_order(order_name)
     if @menu.map{|i| i[:item]}.include?(order_name)
+    # unless @menu.select{|i| i[:item] == order_name}.empty?
       @orders << order_name
-      p "Order #{order_name} added !"
+      p "order #{order_name} added"
     else
-      p "This item is unavailable, sorry!"
+      p "Sorry, this item is unavailable"
     end
   end
-
   def fulfill_order
     p @orders.empty? ? "No orders to fulfill!" : "The #{@orders.shift} is ready!"
   end
-
   def cheapest_item
     p @menu.sort_by { |item| item[:price] }.first[:item]
   end
-
   def drinks_only
-    p @menu.select{ |x| x[:type] == "drink"}.map{|x| x[:item] }
+    p @menu.select{|x| x[:type] == 'drink'}.map{|x| x[:item]}
   end
-
 end
-
-cs1 = Coffeeshop.new("Tokyo Coffee", [{:item => "hot chocolate", :type => "drink", :price => 12}, {:item => "cinnamon roll", :type => "food", :price => 25}, {:item => "iced coffee", :type => "drink", :price => 12}, {:item => "vanilla chai latte", :type => "drink", :price => 32}, {:item => "lemon tea", :type => "drink", :price => 5}])
-
-cs1.add_order("hot cocoa") # "Sorry, this item is unavailable."
-# A Little Spice coffee shop does not sell hot cocoa
-
-cs1.add_order("cinnamon roll") #  "Order added!"
-cs1.add_order("iced coffee") # "Order added!"
-p cs1.orders # ["cinnamon roll", "iced coffee"]
-# All current orders are listed.
-
-cs1.fulfill_order # "The cinnamon roll is ready!"
-cs1.fulfill_order #"The iced coffee is ready!"
-cs1.orders # []
-# All orders have been fulfilled
-
-cs1.cheapest_item # "lemon tea"
-cs1.drinks_only # ["hot chocolate", "lemon tea", "iced coffee", "vanilla chai latte"]
-
+menu = [
+        {item: 'attieke', type: 'food', price: 1500},
+        {item: 'riz', type: 'food', price: 1000},
+        {item: 'foutou', type: 'food', price: 1500},
+        {item: 'coca', type: 'drink', price: 500},
+        {item: 'bissap', type: 'drink', price: 300},
+        {item: 'champagne', type: 'drink', price: 20000}
+      ]
+cs1 = CoffeeShop.new('MamaShelter', menu)
+cs1.add_order('fanta') # => "Sorry, this item is unavailable"
+cs1.add_order('attieke') # => "Order added"
+cs1.add_order('coca') # => "Order added"
+p cs1.orders
+cs1.fulfill_order
+cs1.fulfill_order
+p cs1.orders
+cs1.cheapest_item
+cs1.drinks_only
